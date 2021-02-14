573794dfb030e3b0508d99064036c7578784ba08
marlonhanks <marlon.hanks@kedia.co>
2021-02-14 07:35:27 +0000

    updated to vapash 0.4.3

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,2 +1,7 @@
 hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_USE_OPENSSL=OFF CMAKE_USE_LIBSSH2=OFF CURL_CA_PATH=none)
 hunter_config(Boost VERSION 1.66.0)
+
+hunter_config(vapash VERSION 0.4.3
+    URL https://github.com/marlonhanks/vapash/archive/v0.4.3.tar.gz
+    SHA1 e32b6398269ffb605875cae0c9ef6644c1631633
+)
